36b10fff66bcc41044f7df6eb5057ed80775b1f4
Sridhar <Sridhar@10.0.0.126>
2017-07-17 22:55:08 -0700

    Tested basic localisation features in soubot

--- Node/soubot/app.js
+++ Node/soubot/app.js
@@ -9,7 +9,7 @@ A simple Sourashtra speaking bot that can be run from a console window.
 require('dotenv-extended').load();
 var restify = require('restify');
 var builder = require('botbuilder');
-//var ssml = require('./ssml');
+var ssml = require('./ssml');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -27,14 +27,112 @@ server.post('/api/messages', connector.listen());
 // Create your bot with a function to receive messages from the user
 
 var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("greeting");
+        session.send("instructions");
+        session.beginDialog('localePickerDialog');
+    },
+    function (session) {
+        builder.Prompts.text(session, "text_prompt_ssml", 
+        		{speak: speak(session, 'text_prompt_ssml'),
+        		inputHint: builder.InputHint.ignoringInput 
+        		});
+    },
+    function (session, results) {
+    	session.say('input_response_ssml'+results.response, 
+    			{speak: speak(session, results.response),
+    		inputHint: builder.InputHint.ignoringInput 
+    		});
 
+        //session.send("input_response", results.response);
+        builder.Prompts.number(session, "number_prompt_ssml",
+        		{speak: speak(session, 'number_prompt_ssml'),
+    		inputHint: builder.InputHint.ignoringInput 
+    		});
+    },
+    function (session, results) {
+    	session.say('input_response_ssml'+results.response, 
+    			{speak: speak(session, results.response+''),
+    			inputHint: builder.InputHint.ignoringInput 
+    			});
+       // session.send("input_response", results.response);
+        builder.Prompts.choice(session, "listStyle_prompt", "auto|inline|list|button|none",
+        { speak: speak(session, 'listStyle_prompt_ssml'),
+   		  retrySpeak: speak(session, 'listStyle_prompt_ssml'),
+   		  inputHint: builder.InputHint.expectingInput
+   		});
+    },
+    function (session, results) {
+        // You can use the localizer manually to load a localized list of options.
+        var style = builder.ListStyle[results.response.entity];
+        var options = session.localizer.gettext(session.preferredLocale(), "choice_options");
+        builder.Prompts.choice(session, "choice_prompt", options, { listStyle: style },
+        		 { speak: speak(session, 'choice_prompt_ssml'),
+     		  retrySpeak: speak(session, 'choice_prompt_ssml'),
+     		  inputHint: builder.InputHint.expectingInput
+     		});
+    },
+    function (session, results) {
+    	session.say('choice_response_ssml'+results.response, 
+    			{speak: speak(session, results.response),
+    		inputHint: builder.InputHint.ignoringInput 
+    		});
+       // session.send("choice_response", results.response.entity);
+        builder.Prompts.confirm(session, "confirm_prompt",
+        		 { speak: speak(session, 'confirm_prompt_ssml'),
+   		  retrySpeak: speak(session, 'confirm_prompt_ssml'),
+   		  inputHint: builder.InputHint.expectingInput
+   		});
+    },
+    function (session, results) {
+        // You can use the localizer manually to load prompts from another namespace.
+        var choice = results.response ? 'confirm_yes' : 'confirm_no';
+        session.say('choice_response_ssml'+results.response, 
+    			{speak: speak(session, results.response),
+    		inputHint: builder.InputHint.ignoringInput 
+    		});
+        //session.send("choice_response", session.localizer.gettext(session.preferredLocale(), choice, 'BotBuilder'));
+        builder.Prompts.time(session, "time_prompt",
+        		 { speak: speak(session, 'time_prompt_ssml'),
+     		  retrySpeak: speak(session, 'time_prompt_ssml'),
+     		  inputHint: builder.InputHint.expectingInput
+     		});
+    },
+    function (session, results) {
+    	session.say('time_response_ssml'+results.response, 
+    			{speak: speak(session, results.response),
+    		inputHint: builder.InputHint.ignoringInput 
+    		});
+        //session.send("time_response", JSON.stringify(results.response));
+        
+    	ession.say('demo mujjo', 
+    			{speak: speak(session, 'demo mujjo'),
+    		inputHint: builder.InputHint.ignoringInput 
+    		});
+    	//session.endDialog("demo_finished");
+    }
+]);
+
+    
+//Configure bots default locale and locale folder path.
+
+/*
+ * bot.set('localizerSettings', {
+    botLocalePath: "./locale", 
+    defaultLocale: "en" 
+});
+*/
+
+//Add locale picker dialog 
+
+bot.dialog('localePickerDialog', [
     function (session) {
         // Prompt the user to select their preferred locale
-        builder.Prompts.choice(session, "locale_prompt", 'English|Español|Italiano|Sourashtra',
-        		{ speak: "What's your preferred language",
-        		  retrySpeak: "What's your preferred language",
-        		  inputHint: builder.InputHint.expectingInput
-        		});
+    	 builder.Prompts.choice(session, "locale_prompt", 'English|Español|Italiano|Sourashtra',
+         		{ speak: "What's your preferred language",
+         		  retrySpeak: "What's your preferred language",
+         		  inputHint: builder.InputHint.expectingInput
+         		});
     },
     function (session, results) {
         // Update preferred locale
@@ -50,24 +148,28 @@ var bot = new builder.UniversalBot(connector, [
                 locale = 'it';
                 break;
             case 'Sourashtra':
-                locale = 'saz';
+                locale = 'sa';
                 break;
         }
         session.preferredLocale(locale, function (err) {
             if (!err) {
                 // Locale files loaded
-                session.endDialog('locale_updated');
+                session.endDialog(speak(session, 'locale_updated_ssml'));
+            	console.log('speaking in ' + locale);
+            	
             } else {
                 // Problem loading the selected locale
-                session.erro(err);
+                session.error(err);
+                speak(session,'err_prompt');
             }
         });
     }
 ]);
-    
-//Configure bots default locale and locale folder path.
 
-bot.set('localizerSettings', {
-    botLocalePath: "./locale", 
-    defaultLocale: "en" 
-});
+
+/** Helper function to wrap SSML stored in the prompts file with <speak/> tag. */
+function speak(session, prompt) {
+    var localized = session.gettext(prompt);
+    console.log('Localised speech  ' + localized);
+    return ssml.speak(localized);
+}
